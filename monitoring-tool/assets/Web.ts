35053cd26ed8d633a80cbb1fd25cea76a99aaeef
Lê Tấn <113174963+letan-odc@users.noreply.github.com>
2022-09-23 15:03:27 +0700

    add

---
+++ monitoring-tool/assets/Web.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    clientID: "c276a37f-51bd-4e59-a5ec-0a57a703473b",
+    authority: "https://login.microsoftonline.com/f8a2c782-de47-4307-b690-bee136c44790",
+    redirectUri: "http://localhost:4200/pages-login",
+    tenant: "f8a2c782-de47-4307-b690-bee136c44790",
+  };
+  //https://letan-odc.github.io/monitoring-tool
