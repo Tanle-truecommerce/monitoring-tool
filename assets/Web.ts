396cc159804487eb52166ad63ca90ca47d7b0c88
Lê Tấn <113174963+letan-odc@users.noreply.github.com>
2022-10-04 09:17:17 +0700

    Update T10

--- assets/Web.ts
+++ assets/Web.ts
@@ -1,7 +1,10 @@
 export const environment = {
-    clientID: "c276a37f-51bd-4e59-a5ec-0a57a703473b",
-    authority: "https://login.microsoftonline.com/f8a2c782-de47-4307-b690-bee136c44790",
-    redirectUri: "https://letan-odc.github.io/monitoring-tool/pages-login",
-    tenant: "f8a2c782-de47-4307-b690-bee136c44790",
-  };
-  //https://letan-odc.github.io/monitoring-tool
+  clientID: 'c276a37f-51bd-4e59-a5ec-0a57a703473b',
+  authority:
+    'https://login.microsoftonline.com/f8a2c782-de47-4307-b690-bee136c44790',
+  redirectUri: 'https://letan-odc.github.io/monitoring-tool/pages-login',
+  tenant: 'f8a2c782-de47-4307-b690-bee136c44790',
+  signalrURL: 'http://localhost:8080/signalr',
+  production: false,
+};
+//https://letan-odc.github.io/monitoring-tool
