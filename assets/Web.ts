4562b5802b7f3f5e32fe60c53e2ca1189db0fbc3
Lê Tấn <113174963+Tanle-truecommerce@users.noreply.github.com>
2022-10-06 13:46:18 +0700

    update

--- assets/Web.ts
+++ assets/Web.ts
@@ -2,9 +2,10 @@ export const environment = {
   clientID: 'c276a37f-51bd-4e59-a5ec-0a57a703473b',
   authority:
     'https://login.microsoftonline.com/f8a2c782-de47-4307-b690-bee136c44790',
-  redirectUri: 'https://tanle-truecommerce.github.io/monitoring-tool/pages-login',
+  redirectUri:
+    'https://tanle-truecommerce.github.io/monitoring-tool/pages-login',
   tenant: 'f8a2c782-de47-4307-b690-bee136c44790',
   signalrURL: 'http://localhost:8080/signalr',
   production: false,
 };
-//https://letan-odc.github.io/monitoring-tool
+//https://tanle-truecommerce.github.io/monitoring-tool/
